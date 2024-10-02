f33c4054e8be50c4153c37567d97e6fc5e9ab454
mahmoud kamal <kokolook94@gmail.com>
2024-10-02 08:50:30 +0300

    Initial Commit

---
+++ weather app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather app
+//
+//  Created by Mahmoud on 02/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ weather appTests/weather_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appTests.swift
+//  weather appTests
+//
+//  Created by Mahmoud on 02/10/2024.
+//
+
+import Testing
+@testable import weather_app
+
+struct weather_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
